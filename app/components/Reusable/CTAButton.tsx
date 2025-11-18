b0c0f80ff40303e1a15064b452415cf363f4a6f9
UnaizaZafar <unaizaofficial840@gmail.com>
2025-11-18 17:52:15 +0500

    style: added hover effects to buttons

--- app/components/Reusable/CTAButton.tsx
+++ app/components/Reusable/CTAButton.tsx
@@ -7,9 +7,9 @@ type CTAButtonProps = {
   showArrow?: boolean;
 };
 const variantClasses: Record<ButtonVariants, string> = {
-  "primary-colored": "bg-[#98FF3A] text-black border-1.5 border-transparent",
+  "primary-colored": "bg-[#98FF3A]  hover:bg-black hover:text-[#98FF3A] transition duration-300 text-black border-1.5 border-transparent",
   "primary-black": "text-white bg-black",
-  secondary: "bg-transparent text-white border border-[#F2F2F7]",
+  secondary: "bg-transparent hover:bg-white/30 transition duration-300 text-white border border-[#F2F2F7]",
 };
 export default function CTAButton({
   text,
@@ -18,11 +18,11 @@ export default function CTAButton({
 }: CTAButtonProps) {
   return (
     <Link href="/"
-      className={`flex items-center gap-3 ${variantClasses[variant]}
+      className={`group flex items-center gap-3 ${variantClasses[variant]}
       rounded-xl py-2.5 px-6 font-space-grotesk font-medium justify-center leading-150`}
     >
       {text}
-      {showArrow && <span>{arrow}</span>}
+      {showArrow && <span className="group-hover:-rotate-45 transition duration-300">{arrow}</span>}
     </Link>
   );
 }
