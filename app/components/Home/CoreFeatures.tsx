0460f9535cf6199f6433934a079d0dbf015cc3d3
UnaizaZafar <unaizaofficial840@gmail.com>
2025-11-19 12:56:17 +0500

    fix: improve layout and styling in CoreFeatures and Feature components; adjust spacing and responsiveness

--- app/components/Home/CoreFeatures.tsx
+++ app/components/Home/CoreFeatures.tsx
@@ -5,14 +5,14 @@ import Feature from "../Reusable/Feature";
 
 export default function CoreFeatures() {
   return (
-    <section className="max-w-7xl mx-auto py-28 flex flex-col gap-4 items-center">
+    <section className="max-w-7xl mx-auto px-4 lg:px-6 2xl:px-0 py-10 lg:py-28 flex flex-col gap-4 items-center">
       <SectionHeader
         tagText="Platforms"
         heading="Core Features"
         desc="Powerful tools for paddle club management"
       />
-      <div className="flex gap-12 items-center pt-16">
-        <div className="flex flex-col gap-16 flex-1">
+      <div className="flex max-md:flex-col gap-4 md:gap-7 xl:gap-12 items-center pt-6 md:pt-10 lg:pt-16">
+        <div className="flex flex-col gap-6 md:gap-10 lg:gap-16 flex-1">
           {coreFeaturesData.slice(0, 2).map((item) => (
             <Feature
               key={item.id}
@@ -24,11 +24,12 @@ export default function CoreFeatures() {
         </div>
         <Image
           src={"/images/badminton.webp"}
-          alt="Badmintion"
+          alt="Badminton"
           width={414}
           height={414}
+          className="hidden lg:block"
         />
-        <div className="flex flex-col gap-16 flex-1">
+        <div className="flex flex-col gap-6 md:gap-10 lg:gap-16 flex-1">
           {coreFeaturesData.slice(-2).map((item) => (
             <Feature
               key={item.id}
--- app/components/Reusable/Feature.tsx
+++ app/components/Reusable/Feature.tsx
@@ -5,10 +5,10 @@ type FeaturesProps = {
 };
 export default function Feature({ icon, title, desc }: FeaturesProps) {
   return (
-    <div className="flex flex-col items-center gap-6 text-primary">
+    <div className="flex flex-col items-center gap-4 lg:gap-6 text-primary">
       {icon}
-      <div className="flex flex-col gap-4">
-        <h5 className="font-poppins text-2xl leading-140 -tracking-[1%] text-center">
+      <div className="flex flex-col gap-2 lg:gap-4">
+        <h5 className="font-poppins text-xl lg:text-2xl leading-140 -tracking-[1%] text-center">
           {title}
         </h5>
         <p className="font-space-grotesk leading-150 text-center">{desc}</p>
